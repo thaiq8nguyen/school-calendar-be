1ba0927e66ee416cc60b036a19dbecb34be80d6c
ch04937 <97hernandez.c@gmail.com>
2019-10-28 12:29:01 -0700

    routes need migration to complete

--- api/server.js
+++ api/server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+//setting up routes
+const userRouter = require('../routes/user-router');
+
+//routes
+server.use('/api/users', userRouter);
 
 //testing server
 server.get('/', (req, res) => {
