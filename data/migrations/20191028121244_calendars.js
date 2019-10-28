fd944302f64b91099376ae8469e3ae1f68c70a81
bharti3bk <bharti3bk@gmail.com>
2019-10-28 16:32:59 -0700

    Completed Migrations

--- data/migrations/20191028121244_calendars.js
+++ data/migrations/20191028121244_calendars.js
@@ -9,10 +9,66 @@ exports.up = function(knex) {
       table.boolean('isAdmin').notNullable().defaultTo(false);
       table.integer('userId').unique();
       
+  }) 
+  // .createTable("userCalendars" , table => {
+  //   table.increments();
+  //   table.integer("userCalendarId")
+  //   table.integer("	userId").unique()
+  //   table.integer("CalendarId")
+  // })  
+  .createTable("Calendars" , table => {
+     table.increments();
+     table.string("calendarName")
+     table.integer("calendarId")
+  })    
+  .createTable("Events" , table => {
+    table.increments();
+    table.integer("eventId")
+    table.increments('eventName')
+    table.increments('eventInfo')
   })
-};
 
+  .createTable('userCalendars', table => {
+    table.increments()
+    table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('usersLogin')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+   table
+        .integer('calenderId')
+        .unsigned()
+        .references('id')
+        .inTable('Calendars')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+})  
+.createTable('CalendarEvents' , table => {
+table.increments()
+table
+    .integer('calenderId')
+    .unsigned()
+    .references('id')
+    .inTable('Calendars')
+    .onDelete('CASCADE')
+    .onUpdate('CASCADE');
+table 
+    .integer('eventsId') 
+    .unsigned()
+    .references('id')
+    .inTable('Events')
+    .onDelete('CASCADE')
+    .onUpdate('CASCADE');
+})
+}
 exports.down = function(knex) {
   return knex.schema
-  .dropTableIfExists("usersLogin");
+  .dropTableIfExists("usersLogin")
+  .dropTableIfExists("userCalendars") 
+  .dropTableIfExists("calendars") 
+  .dropTableIfExists("CalendarEvent") 
+  .dropTableIfExists("Events") 
+  
 };
