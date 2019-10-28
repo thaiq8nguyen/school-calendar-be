1409ac4d4f6b0603cec69611eb1a94ecb08613c2
ch04937 <97hernandez.c@gmail.com>
2019-10-28 12:18:33 -0700

    creating users table

--- data/migrations/20191028121244_calendars.js
+++ data/migrations/20191028121244_calendars.js
@@ -1,6 +1,11 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema 
+  .createTable('users', tbl => {
+      tbl.increments();
+      tbl
+        .string('username', 255)
+  })
 };
 
 exports.down = function(knex) {
